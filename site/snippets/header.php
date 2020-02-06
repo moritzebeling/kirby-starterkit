2f0621ad05aa024104312463b12c5fb205fa7153
moritzebeling <git@moritzebeling.com>
2020-02-06 13:44:22 +0100

    load correct css files

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,16 +4,14 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title><?= $site->title() ?> • <?= $page->title() ?></title>
-  
+
   <link rel="canonical" href="<?= $page->url() ?>" />
 
-  <meta name="description" content="">
-  <meta name="keywords" content="">
-  <meta name="subject" content="">
+  <meta name="description" content="<?= $page->description()->html() ?>">
+  <meta name="keywords" content="<?= $page->tags()->html() ?>">
   <meta name="Generator" content="Kirby 3 (https://getkirby.com), Moritz Ebeling (https://moritzebeling.com)">
 
   <?= css([
-    'assets/css/normalize.css',
     'assets/css/global.css',
     '@auto'
   ]) ?>
@@ -21,15 +19,16 @@
   <?php
   /**
    * Resources:
+   * https://gist.github.com/lancejpollard/1978404
    * https://htmlhead.dev/#recommended-minimum
-   * 
+   *
    * https://realfavicongenerator.net
    * https://jsonld.com/json-ld-generator/
    * https://ogp.me
    * https://dev.twitter.com/cards/overview
-   * 
+   *
    * https://css-tricks.com/prefetching-preloading-prebrowsing/
-   * 
+   *
    * <link rel="alternate" href="<?= $page->url('en') ?>" hreflang="en">
    * <link rel="alternate" href="<?= $page->url('de') ?>" hreflang="de">
    */
@@ -51,4 +50,3 @@
       </nav>
 
     </header>
-
