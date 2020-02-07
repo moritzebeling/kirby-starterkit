028179c843ee297541e50814e79d4e7dc5b402a8
moritzebeling <git@moritzebeling.com>
2020-02-07 10:20:05 +0100

    setup de and en languages

---
+++ site/languages/de.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'code' => 'de',
+  'default' => false,
+  'direction' => 'ltr',
+  'locale' => 'de_DE',
+  'name' => 'Deutsch',
+  'url' => 'de',
+  'slug' => [
+    'ß' => 'sz',
+    'ä' => 'ae',
+    'ö' => 'oe',
+    'ü' => 'ue',
+  ]
+];
---
+++ site/languages/en.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+  'code' => 'en',
+  'default' => true,
+  'direction' => 'ltr',
+  'locale' => 'en_US',
+  'name' => 'English',
+  'url' => '/',
+  'slug' => [
+    't’s' => 'ts',
+    'n’t' => 'nt',
+  ]
+];
