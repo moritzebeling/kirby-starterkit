ccbe36bdfe4430ac91a579de0697b5bc89bbce49
moritzebeling <git@moritzebeling.com>
2020-05-04 15:05:45 +0200

    add basic jsonld

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -34,6 +34,22 @@
    */
   ?>
 
+  <?php $jsonld = [
+  	'@context' => 'https://schema.org/',
+  	'@type' => 'WebSite',
+  	'copyrightYear' => date('Y'),
+  	'name' => $site->title()->value(),
+  	'url' => $site->url(),
+  	'inLanguage' => []
+  ];
+  foreach( $site->kirby()->languages() as $lang ){
+    $jsonld['inLanguage'] = [
+  		'@type' => 'Language',
+  		'name' => $lang->name(),
+  	];
+  } ?>
+  <script type="application/ld+json"><?= json_encode($jsonld) ?></script>
+
 </head>
 <!-- This website was made by Moritz Ebeling (https://moritzebeling.com) -->
 <body>
