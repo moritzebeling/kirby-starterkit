89d97903664e153739ece030b98a65fc34a95f81
moritzebeling <git@moritzebeling.com>
2020-05-04 15:53:08 +0200

    favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -16,21 +16,27 @@
     '@auto'
   ]) ?>
 
+  <?php $fav = 'assets/favicon/'; ?>
+  <link rel="shortcut icon" href="<?= $fav; ?>favicon.ico">
+  <?php foreach([16,32,48,64,96,128,180,256,512,1024] as $s ): ?>
+    <link rel="icon" type="image/png" href="<?= $fav; ?>favicon.png" sizes="<?= $s.'x'.$s ?>">
+  <?php endforeach; ?>
+  <link rel="icon" type="image/svg+xml" href="<?= $fav; ?>favicon.svg" sizes="any">
+  <link rel="apple-touch-icon-precomposed" sizes="180x180" href="<?= $fav; ?>apple-touch-icon.png">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="<?= $fav; ?>mstile-144x144.png">
+
   <?php
   /**
    * Resources:
    * https://gist.github.com/lancejpollard/1978404
    * https://htmlhead.dev/#recommended-minimum
    *
-   * https://realfavicongenerator.net
-   * https://jsonld.com/json-ld-generator/
    * https://ogp.me
    * https://dev.twitter.com/cards/overview
    *
    * https://css-tricks.com/prefetching-preloading-prebrowsing/
    *
-   * <link rel="alternate" href="<?= $page->url('en') ?>" hreflang="en">
-   * <link rel="alternate" href="<?= $page->url('de') ?>" hreflang="de">
    */
   ?>
 
@@ -38,7 +44,9 @@
     <link rel="alternate" href="<?= $page->url($lang->code()) ?>" hreflang="<?= $lang->code() ?>">
   <?php endforeach; ?>
 
-  <?php $jsonld = [
+  <?php
+  // https://jsonld.com/json-ld-generator/
+  $jsonld = [
   	'@context' => 'https://schema.org/',
   	'@type' => 'WebSite',
   	'copyrightYear' => date('Y'),
