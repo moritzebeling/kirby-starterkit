b9511f70399b13b5dbf003813160891aecaa6964
moritzebeling <git@moritzebeling.com>
2021-01-29 19:43:16 +0100

    simplify favicon settings

--- site/snippets/meta/favicon.php
+++ site/snippets/meta/favicon.php
@@ -1,18 +1,5 @@
-<?php
-
-$dir = '/assets/favicon';
-
-$sizes = [16,32,64,128,256,512,1024];
-
-?>
-<link rel="shortcut icon" type="image/x-icon" href="<?= $dir; ?>/favicon.ico">
-
-<?php foreach( $sizes as $size ): ?>
-  <link rel="icon" type="image/png" href="<?= $dir; ?>/favicon-<?= $size ?>.png" sizes="<?= $size.'x'.$size ?>">
+<link rel="shortcut icon" type="image/x-icon" href="/assets/favicon/favicon.ico">
+<link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" sizes="any">
+<?php foreach( [64,128,256,512,1024] as $size ): ?>
+	<link rel="icon" type="image/png" href="/assets/favicon/favicon-<?= $size ?>.png" sizes="<?= $size.'x'.$size ?>">
 <?php endforeach; ?>
-
-<link rel="icon" type="image/svg+xml" href="<?= $dir; ?>/favicon.svg" sizes="any">
-
-<link rel="apple-touch-icon-precomposed" sizes="180x180" href="<?= $dir; ?>/apple-touch-icon.png">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="<?= $dir; ?>/mstile-144x144.png">
