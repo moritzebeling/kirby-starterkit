b99022c9024aa4fbafa295d1116c4974feb61414
moritzebeling <git@moritzebeling.com>
2021-03-15 23:38:15 +0100

    working on frontend setup

---
+++ src/components/Image.svelte
@@ -0,0 +1,41 @@
+<script>
+
+    export let image;
+
+    function srcset( set ){
+        let s = '';
+        for (const size of set ) {
+            s += `${size.url} ${size.width}w, `;
+        }
+        return s;
+    }
+
+</script>
+
+<div class="img">
+    <img alt={image.alt}
+        src={image.url}
+        class="lazyload"
+        data-sizes="auto"
+        data-src={image.url}
+        data-srcset={srcset(image.srcset)} />
+</div>
+
+<style lang="scss">
+
+    div {
+        position: relative;
+        &:after {
+            content: '';
+            background-color: #000;
+            opacity: 0.03;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            position: absolute;
+            z-index: 2;
+        }
+    }
+
+</style>
--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,16 +1,21 @@
+
 <script>
   import { Link } from "svelte-routing";
 
+  let className = "";
+	export { className as class };
   export let to = "";
 
   function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
     const isActive = href === "/" ? isCurrent : isPartiallyCurrent || isCurrent;
 
-    // The object returned here is spread on the anchor element's attributes
-    if (isActive) {
-      return { class: "active" };
-    }
-    return {};
+    className = Array.isArray( className ) ? className : className.split(' ');
+
+    let active = isActive ? ' active' : '';
+
+    return {
+      class: className.join(' ') + active
+    };
   }
 </script>
 
