26bc83bdbf6d3901677d5c0ac47fa46b10f2e261
moritzebeling <git@moritzebeling.com>
2023-06-03 21:15:41 +0200

    implement meta fields

--- site/snippets/html.php
+++ site/snippets/html.php
@@ -11,17 +11,7 @@ use Kirby\Filesystem\F;
 <html lang="<?= $kirby->language()?->code() ?? $site->lang() ?>">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    <title><?= $page->title() ?> | <?= $site->title() ?></title>
-
-    <?php foreach ($kirby->languages() as $language) :
-        if ($language->code() !== $kirby->language()->code()) : ?>
-            <link rel="alternate" hreflang="' . $language->code() . '" href="' . $page->url($language->code()) . '" />
-        <?php endif;
-    endforeach ?>
-
+    <?php snippet('meta') ?>
     <?php snippet('favicon') ?>
 
     <?php if ($host = F::read('assets/dist/hot')) : ?>
